1c6de04918145736a4fb7dc9ff29a528b10cbb68
camila <camiromero100@gmail.com>
2022-06-15 10:39:12 -0300

    add: Add a count message

--- tests/integration/test_gcloud/test_functionality/test_max_messages.py
+++ tests/integration/test_gcloud/test_functionality/test_max_messages.py
@@ -47,6 +47,7 @@ tags:
     - scan
     - maximum
 '''
+from itertools import count
 import os
 import sys
 import pytest
@@ -70,6 +71,7 @@ interval = '25s'
 pull_messages_timeout = global_parameters.default_timeout + 60
 pull_on_start = 'no'
 max_messages = 100
+count_message = 0
 logging = 'info'
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
