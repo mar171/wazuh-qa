2085c65786473a9b758fc0cbabb8702ede7da0ce
fedepacher <fede_pacher@hotmail.com>
2022-08-08 13:38:15 -0300

    dfff

--- tests/system/conftest.py
+++ tests/system/conftest.py
@@ -16,21 +16,17 @@ def clean_environment(test_infra_agents, test_infra_managers, host_manager):
 
 def pytest_addoption(parser):
     import os
-    # inventory_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
-    #                   'provisioning', 'one_manager_agent', 'inventory.yml')
+    inventory_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                      'provisioning', 'one_manager_agent', 'inventory.yml')
     parser.addoption(
         '--inventory',
-        action='append',
-        default=[],
+        action='store_true',
+        default=[inventory_path],
         help='Add inventory path'
     )
 
 
 def pytest_generate_tests(metafunc):
-    import os
-    inventory_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
-                      'provisioning', 'one_manager_agent', 'inventory.yml')
     if 'inventory' in metafunc.fixturenames:
         metafunc.parametrize('inventory', metafunc.config.getoption('inventory'))
-    else:
-        metafunc.parametrize('inventory', inventory_path)
+
