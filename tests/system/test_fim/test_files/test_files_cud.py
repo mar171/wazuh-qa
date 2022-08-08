bb052889e5f4a7303c550758195c89b5d96c7de4
fedepacher <fede_pacher@hotmail.com>
2022-08-08 15:47:33 -0300

    scs

--- tests/system/test_fim/test_files/test_files_cud.py
+++ tests/system/test_fim/test_files/test_files_cud.py
@@ -52,9 +52,9 @@ from test_fim import create_folder_file, wait_for_fim_scan_end
 # Hosts
 testinfra_hosts = ["wazuh-manager", "wazuh-agent1"]
 
-# inventory_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
-#                               'provisioning', 'one_manager_agent', 'inventory.yml')
-# host_manager = HostManager(inventory_path)
+inventory_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                              'provisioning', 'one_manager_agent', 'inventory.yml')
+host_manager = HostManager(inventory_path)
 local_path = os.path.dirname(os.path.abspath(__file__))
 messages_path = [os.path.join(local_path, 'data/messages.yml'),
                  os.path.join(local_path, 'data/delete_message.yml'),
@@ -65,7 +65,7 @@ scheduled_mode = 'testdir1'
 
 @pytest.mark.parametrize('case', ['add', 'modify', 'delete'])
 @pytest.mark.parametrize('folder_path', ['testdir1', 'testdir2', 'testdir3'])
-def test_file_cud(inventory, folder_path, case):
+def test_file_cud(folder_path, case):
     '''
 
     description:  The test will monitor a directory.
@@ -97,7 +97,6 @@ def test_file_cud(inventory, folder_path, case):
         - realtime
         - who_data
     '''
-    host_manager = HostManager(inventory)
     messages = messages_path[0]
     enviroment_files = [('wazuh-manager', os.path.join(WAZUH_LOGS_PATH, 'ossec.log')),
                         ('wazuh-agent1', os.path.join(WAZUH_LOGS_PATH, 'ossec.log'))]
@@ -109,7 +108,7 @@ def test_file_cud(inventory, folder_path, case):
 
     # Check if the scan monitors end
     if (folder_path == scheduled_mode):
-        wait_for_fim_scan_end(HostMonitor, inventory, messages_path[2], tmp_path)
+        wait_for_fim_scan_end(HostMonitor, inventory_path, messages_path[2], tmp_path)
 
     if (case == 'modify'):
         host_manager.modify_file_content(host='wazuh-agent1', path=folder_path, content=folder_path)
@@ -120,7 +119,7 @@ def test_file_cud(inventory, folder_path, case):
 
     try:
         # Run the callback checks for the ossec.log
-        HostMonitor(inventory_path=inventory,
+        HostMonitor(inventory_path=inventory_path,
                     messages_path=messages,
                     tmp_path=tmp_path).run()
     finally:
